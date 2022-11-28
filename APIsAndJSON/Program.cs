d6eedc9f4cea9408c34c5f744d8482a72dd2ea89
Yvette Lerma <lerma.yvette@hotmail.com>
2022-11-27 23:21:06 -0700

    code added

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace APIsAndJSON
+﻿using Newtonsoft.Json.Linq;
+
+var client = new HttpClient();
+
+var kanyeURL = "http://api.kanye.rest/";
+var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+var cont = true;
+do
 {
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+
+    var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+
+    var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+    Console.WriteLine($"Kanye says: \"{kanyeQuote}\"\n");
+
+
+    var ronResponse = client.GetStringAsync(ronURL).Result;
+
+    var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+    Console.WriteLine($"Ron says: {ronQuote}\n");
+
+    Console.WriteLine("Continue? Y or N?");
+    cont = (Console.ReadLine().ToLower() == "n") ? false : true;
+
+} while (cont);
